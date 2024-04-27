30bc6a91b1095027aa745bb65c034761b77d7312
“inna_tssss” <inna.tspin11@gmail.com>
2024-04-27 19:25:39 +0200

    task 2

--- script.js
+++ script.js
@@ -14,11 +14,40 @@ function delayedResolve() {
   return new Promise((resolve, reject) => {
     const delay = Math.floor(Math.random() * 5) + 1;
     setTimeout(() => {
-      resolve(`Promise resolved after ${delay} seconds`);
+      resolve(`Promise resolved after ${delay} second`);
     }, delay * 1000); // seconds to milliseconds
   });
 }
 
 delayedResolve().then((message) => {
   console.log(message);
+});
+
+// Ask a user to submit an artist name and song and after 2 seconds add it to the screen
+$(document).ready(function () {
+  $("#songForm").submit(function (event) {
+    event.preventDefault();
+
+    const artistName = $("#artistName").val();
+    const songTitle = $("#songTitle").val();
+
+    // validation
+    if (songTitle === "" || artistName === "") {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    // 2 seconds delay
+    setTimeout(function () {
+      let newRow = $("<tr>");
+      let cell1 = $("<td>").text(songTitle);
+      let cell2 = $("<td>").text(artistName);
+
+      newRow.append(cell1, cell2);
+      $("#songTable tbody").append(newRow);
+
+      $("#songTitle").val("");
+      $("#artistName").val("");
+    }, 2000);
+  });
 });
